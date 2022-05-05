048b1a0a16b5215106929e31f819aefb42d481b2
hardikarora2311 <70485045+hardikarora2311@users.noreply.github.com>
2022-05-05 12:30:30 +0530

    styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
         <hr></hr>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</p>
       </div>
-      <ServiceList/>
+      <ServiceList className="service-list"/>
     </section>
 
     <section className="work-section">
